c365fd61a4532a664d38e51ae8b71ec58bb5eb0c
Wille-J <82776900+Wille-J@users.noreply.github.com>
2022-02-13 00:31:11 +0100

    Created the main class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,26 +1,61 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
 
 	public static void main(String[] args) {
+		AddressBook ab = new AddressBook();
 		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("Write your name, address and phonenumber.");
 
-		while(true) {
-			System.out.println("Whould you like to ad another person?");
-				if(scanner.nextLine().equals("yes")) {
-					createPerson(scanner, addressbook);
-					
+		String search;
+		int choice;
+
+		try {
+			while (true){
+				System.out.println("Enter any of the following numbers: \n"
+								 + "1 - Add \n"
+								 + "2 - Search \n" + "3 - Delete \n"
+								 + "4 - Save & Exit \n");
+				choice = scanner.nextInt();
+				
+				if(choice >= 1 && choice <= 4) {
+				switch (choice) {
+				case 1:
+					ab.addPerson();
+					break;
+				case 2:
+					System.out.println("Who do you want to search for?");
+					scanner.nextLine();
+					search = scanner.nextLine();
+					ab.searchPerson(search);
+					break;
+				case 3:
+					System.out.println("Who do you want to delete?");
+					scanner.nextLine();
+					search = scanner.nextLine();
+					ab.deletePerson(search);
+					break;
+				case 4:
+					ab.savePersons();
+					System.out.println("Your entered contacts have been saved. \n"
+									 + "Goodbye! \n");
+					System.exit(0);
+				default:
+					System.out.println("Enter a choice from 1 to 4. \n");
+					break;
 				}
-				break;
-		}		
-		
-		
-		System.out.println(addressbook);
-	}
+				}
+				else {
+					System.out.println("You entered a incorrect input!");
+					continue;
+				}
+				
+			}
+			}
+			catch (InputMismatchException e) {
+				System.out.println("You entered a incorrect input! \n"
+								 + "The program will now exit, goodbye!");
+			}
 	
-
+	}
 }
