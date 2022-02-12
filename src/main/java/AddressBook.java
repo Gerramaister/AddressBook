f99fbd7482aa950cebd3cf94c85acbaa59875b11
oskarold <Oskargj.johansson@gmail.com>
2022-02-12 17:50:32 +0100

    Added the savePersons method

--- src/main/java/AddressBook.java
+++ src/main/java/AddressBook.java
@@ -1,3 +1,6 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -13,18 +16,32 @@ public class AddressBook {
 	public void addPerson(PersonInfo person) {
 	
 	}
-	
-	
 	public PersonInfo createPerson(Scanner scanner, List<PersonInfo> addressbook) {
 		PersonInfo person = new PersonInfo(scanner.nextLine(), scanner.nextLine(), scanner.nextLine());
 		this.addressbook.add(person);
 		return person;
 	}
 	
+	public void savePersons() {
+		try {
+			PersonInfo p;
+			String line;
+			FileWriter fw = new FileWriter("persons.txt");
+			PrintWriter pw = new PrintWriter(fw);
+			for(int i = 0; i < persons.size(); i++) {
+				p = (PersonInfo) persons.get(i);
+				line = p.getName() + "," + p.getAddress() + p.getPhoneNumber();
+				//Write line to persons.txt
+				pw.println(line);
+			}
+			pw.flush();
+			pw.close();
+			fw.close();
+		}catch(IOException ioEx) {
+			System.out.println(ioEx);
+		}
+		
+	}
 	
 	
-	
-	
-	
-
 }
