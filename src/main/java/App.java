dfd5609831275d183047e1ac3e739d1a8bd9e94d
Jonathan Gerard <jonathan.gerard@hotmail.se>
2022-02-12 15:42:21 +0100

    trying to loop a list of PersonInfo

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,24 +3,40 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
+	
+	private List<PersonInfo> addressbook = new ArrayList<PersonInfo>();
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Write your name, address and phonenr.");
+		System.out.println("Write your name, address and phonenumber.");
 		//PersonInfo person1 = new PersonInfo(scanner.nextLine(), scanner.nextLine(), scanner.nextLine());
-		PersonInfo person2 = new PersonInfo("Kalle", "Hantverkarvägen 2", "+425678");
-		PersonInfo person3 = new PersonInfo("Lalle", "Hantverkarvägen 3", "+425678");
-		PersonInfo person4 = new PersonInfo("Halle", "Hantverkarvägen 4", "+425678");
-		PersonInfo person5 = new PersonInfo("Falle", "Hantverkarvägen 6", "+425678");
+		//PersonInfo person2 = new PersonInfo("Kalle", "Hantverkarvägen 2", "+425678");
+		//PersonInfo person3 = new PersonInfo("Lalle", "Hantverkarvägen 3", "+425678");
+		//PersonInfo person4 = new PersonInfo("Halle", "Hantverkarvägen 4", "+425678");
+		//PersonInfo person5 = new PersonInfo("Falle", "Hantverkarvägen 6", "+425678");
+		while(true) {
+			System.out.println("Whould you like to ad another person?");
+				if(scanner.nextLine().equals("yes")) {
+					createPerson(scanner, addressbook);
+					
+				}
+				break;
+		}
+		createPerson(scanner);
 		
+		List<PersonInfo> addressbook = new ArrayList<PersonInfo>();
 		
-		List<PersonInfo> addressbook = new ArrayList<>();
-		
-		addressbook.add(person2);
-		addressbook.add(person3);
-		addressbook.add(person4);
-		addressbook.add(person5);
+		//addressbook.add(person);
+		//addressbook.add(person3);
+		//addressbook.add(person4);
+		//addressbook.add(person5);
 		
 		System.out.println(addressbook);
+	}
+	
+	public static PersonInfo createPerson(Scanner scanner, List<PersonInfo> addressbook) {
+		PersonInfo person = new PersonInfo(scanner.nextLine(), scanner.nextLine(), scanner.nextLine());
+		addressbook.add(person);
+		return person;
 	}
 }
