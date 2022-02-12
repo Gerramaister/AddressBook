d5a4eb5cb1dd2bfd158c709918fe4b90eaae6a9b
oskarold <Oskargj.johansson@gmail.com>
2022-02-12 17:50:32 +0100

    Added the savePersons method and removed setters.

--- src/main/java/PersonInfo.java
+++ src/main/java/PersonInfo.java
@@ -1,43 +1,30 @@
 
 public class PersonInfo {
-	String name;
-	String address;
-	String phoneNumber;
+	private String name;
+	private String address;
+	private String phoneNumber;
 	
 	public PersonInfo(String n, String a, String p) {
-		name = n;
-		address = a;
-		phoneNumber = p;
+		this.name = n;
+		this.address = a;
+		this.phoneNumber = p;
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public String getAddress() {
 		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
 	}
 
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
 
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-
 	@Override
 	public String toString() {
-		return "PersonInfo [name=" + name + ", address=" + address + ", phoneNumber=" + phoneNumber + " \n]";
+		return "--PersonInfo-- Name: " + name + ", Address: " + address + ", Phonenumber: " + phoneNumber + " ";
 	}
-	
-	
+		
 }
