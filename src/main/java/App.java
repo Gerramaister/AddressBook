9e0b032ccf5f6c2f32e2c97cfe7f7fd1287a1319
oskarold <Oskargj.johansson@gmail.com>
2022-02-12 14:50:21 +0100

    Created PersonInfo Class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,10 @@
+import java.util.Scanner;
 
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		PersonInfo person = new PersonInfo(scanner.nextLine(), scanner.nextLine(), scanner.nextLine());
 	}
 
 }
