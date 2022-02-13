c2c95184993b694903e6331d480ca56667adff6c
Jonathan Gerard <jonathan.gerard@hotmail.se>
2022-02-13 14:29:44 +0100

    added equals and hashcode then removed it

--- src/main/java/PersonInfo.java
+++ src/main/java/PersonInfo.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 
 public class PersonInfo {
 	private String name;
@@ -25,6 +26,23 @@ public class PersonInfo {
 	@Override
 	public String toString() {
 		return "--PersonInfo-- Name: " + name + ", Address: " + address + ", Phonenumber: " + phoneNumber + " ";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PersonInfo other = (PersonInfo) obj;
+		return Objects.equals(name, other.name);
 	}
 		
 }
